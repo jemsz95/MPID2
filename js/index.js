3dd63cd5e0f10baaa63aeba7ad9a4d6a9f114a45
Javier Meza <javenmeza@gmail.com>
2015-03-22 09:40:17 -0600

    Send real form

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@
                 $.ajax({
                     url: "ajax-tesseract.php", // Url to which the request is send
                     type: "POST",             // Type of request to be send, called as method
-                    data: new FormData(this), // Data sent to server, a set of key/value pairs (i.e. form fields and values)
+                    data: formData, // Data sent to server, a set of key/value pairs (i.e. form fields and values)
                     contentType: false,       // The content type used when sending data to the server.
                     cache: false,             // To unable request pages to be cached
                     processData:false,        // To send DOMDocument or non processed data file it is set to false
