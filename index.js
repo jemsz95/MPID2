7630d1d754e23cd107b8ee2f010fc0e9618a0a99
Javier Meza <javenmeza@gmail.com>
2015-03-21 23:22:10 -0600

    Prueba de camara

---
+++ index.js
@@ -0,0 +1,39 @@
+//Acceder webcam
+var video = document.getElementById('webcam');
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+w = canvas.width;
+h = canvas.height;
+
+navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia ||
+                          navigator.mozGetUserMedia || navigator.msGetUserMedia);
+   if (navigator.getUserMedia) {
+      navigator.getUserMedia(
+         {
+            video:true
+         },
+         function(stream) {
+             video.src = window.URL.createObjectURL(stream);
+         },
+         function(error) {
+             alert('Su navegador no permite acceder a la camara web o no existe una camara web');
+         }
+     );
+   }
+   else {
+      alert('Su navegador no permite acceder a la camara web o no existe una camara web');
+    }
+
+//Draw feed on canvas
+timer = setInterval( function() {
+    context.drawImage(video, 0, 0, w, h);
+}, 34);
+
+
+//Set up para detectar las credenciales
+var corners = [];
+
+var cols = 32;
+var rows = num_corners
+var descriptors = new jsfeat.matrix_t(cols, rows, jsfeat.U8_t | jsfeat.C1_t);
+jsfeat.orb.describe(img_u8:matrix_t, corners:Array, num_corners, descriptors:matrix_t);
