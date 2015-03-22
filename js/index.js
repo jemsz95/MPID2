89925f20147728135f44946b4ce94a82013977f7
Javier Meza <javenmeza@gmail.com>
2015-03-22 08:44:45 -0600

    Fix ajax error

--- js/index.js
+++ js/index.js
@@ -584,7 +584,7 @@
                         contentType: false,       // The content type used when sending data to the server.
                         cache: false,             // To unable request pages to be cached
                         processData:false,        // To send DOMDocument or non processed data file it is set to false
-                        success: successCallback
+                        success: successCallback,
                         error: errorCallback
                     });
                 }
