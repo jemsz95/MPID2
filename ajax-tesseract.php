a060cd2847ce5978de9f7b2dccc46d8f4968b16d
Javier Meza <javenmeza@gmail.com>
2015-03-22 09:06:46 -0600

    Fix php error

--- ajax-tesseract.php
+++ ajax-tesseract.php
@@ -8,7 +8,7 @@
     $api->Init(".","eng",$mode_or_oem=OEM_DEFAULT);
     $api->SetPageSegMode(PSM_AUTO);
 
-    $image = $_FILES['image']['tmp_name']
+    $image = $_FILES['image']['tmp_name'];
     $string = ProcessPagesFileStream($image,$api);
 
     $result = json_encode(array('string' => $string));
