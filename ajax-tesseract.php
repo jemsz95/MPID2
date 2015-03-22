b9b44bfeb48099a0fae6a624f32fef698a7f3896
Javier Meza <javenmeza@gmail.com>
2015-03-22 09:19:22 -0600

    fart

--- ajax-tesseract.php
+++ ajax-tesseract.php
@@ -1,9 +1,6 @@
 <?php
     include '../php-tesseract/tesseract.php';
 
-    error_reporting(-1);
-    ini_set('display_errors', 'On');
-
     if(!empty($_FILES)) {
         //$api= new TessBaseAPI;
         //$api->Init(".","eng",$mode_or_oem=OEM_DEFAULT);
@@ -12,14 +9,14 @@
         //$image = $_FILES['image']['tmp_name'];
         //$string = ProcessPagesFileStream($image,$api);
 
-        $string = 'something';
+        //$string = 'something';
 
-        $result = array('string' => $string);
-        $ajax = json_encode($result);
+        //$result = array('string' => $string);
+        //$ajax = json_encode($result);
     } else {
-        $ajax = 'No files!';
+        //$ajax = 'No files!';
     }
 
 
-    echo $ajax;
+    echo 'hello';
 ?>
