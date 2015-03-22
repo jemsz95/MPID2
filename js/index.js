1deba73d4d148dd0b61990b7b4d465d6226a574d
Javier Meza <javenmeza@gmail.com>
2015-03-22 09:50:46 -0600

    ghjk

--- js/index.js
+++ js/index.js
@@ -13,6 +13,9 @@
                     url: "ajax-tesseract.php", // Url to which the request is send
                     type: "POST",             // Type of request to be send, called as method
                     data: formData, // Data sent to server, a set of key/value pairs (i.e. form fields and values)
+                    contentType: false,       // The content type used when sending data to the server.
+                    cache: false,             // To unable request pages to be cached
+                    processData:false,       // To send DOMDocument or non processed data file it is set to false
                     success: successCallback,
                     error: errorCallback
                 });
