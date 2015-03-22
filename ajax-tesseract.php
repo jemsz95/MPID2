88aeff0efb872f681dad92c8da86a5d903ffd595
Javier Meza <javenmeza@gmail.com>
2015-03-22 09:30:41 -0600

    UnCommented

--- ajax-tesseract.php
+++ ajax-tesseract.php
@@ -1,22 +1,22 @@
 <?php
-    //include '../php-tesseract/tesseract.php';
+    include '/tesseract/tesseract.php';
 
     if(!empty($_FILES)) {
-        //$api= new TessBaseAPI;
-        //$api->Init(".","eng",$mode_or_oem=OEM_DEFAULT);
-        //$api->SetPageSegMode(PSM_AUTO);
+        $api= new TessBaseAPI;
+        $api->Init(".","eng",$mode_or_oem=OEM_DEFAULT);
+        $api->SetPageSegMode(PSM_AUTO);
 
-        //$image = $_FILES['image']['tmp_name'];
-        //$string = ProcessPagesFileStream($image,$api);
+        $image = $_FILES['image']['tmp_name'];
+        $string = ProcessPagesFileStream($image,$api);
 
-        //$string = 'something';
+        $string = 'something';
 
-        //$result = array('string' => $string);
-        //$ajax = json_encode($result);
+        $result = array('string' => $string);
+        $ajax = json_encode($result);
     } else {
-        //$ajax = 'No files!';
+        $ajax = 'No files!';
     }
 
 
-    echo 'hello';
+    echo $ajax;
 ?>
