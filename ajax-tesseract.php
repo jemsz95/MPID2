1f7b4802a48912943cb2e2a9555ff1094f644446
Javier Meza <javenmeza@gmail.com>
2015-03-22 09:16:08 -0600

    MOAR DEBUG

--- ajax-tesseract.php
+++ ajax-tesseract.php
@@ -5,9 +5,9 @@
     ini_set('display_errors', 'On');
 
     if(!empty($_FILES)) {
-        $api= new TessBaseAPI;
-        $api->Init(".","eng",$mode_or_oem=OEM_DEFAULT);
-        $api->SetPageSegMode(PSM_AUTO);
+        //$api= new TessBaseAPI;
+        //$api->Init(".","eng",$mode_or_oem=OEM_DEFAULT);
+        //$api->SetPageSegMode(PSM_AUTO);
 
         //$image = $_FILES['image']['tmp_name'];
         //$string = ProcessPagesFileStream($image,$api);
